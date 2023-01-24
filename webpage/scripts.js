60ab1286498f343bee1b187cfdc2c334505d070b
Adam Magloire <adam.magloire329@gmail.com>
2023-01-24 15:30:48 -0500

    Add Webpage

---
+++ webpage/scripts.js
@@ -0,0 +1,16 @@
+
+
+
+
+const darkModeBtn = document.getElementById("dark-mode");
+// darkModeBtn.addEventListener("click", function() {
+//     body.style.background = "#654ea3";
+
+// })
+
+    darkModeBtn.addEventListener('click', function onClick() {
+        document.body.style.backgroundImage = 'linear-gradient(to right, #1d2b64, #f8cdda)';
+    })
+
+
+//make this work
